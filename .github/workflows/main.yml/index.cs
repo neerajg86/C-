f37870e521913268a0d1e61ffac3dda3728840ee
neerajg86 <57834703+neerajg86@users.noreply.github.com>
2020-10-08 15:07:45 +0530

    index
    commit

---
+++ .github/workflows/main.yml/index.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
